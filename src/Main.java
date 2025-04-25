16f8d020cc5d8ca8b0dbef74f11a8e9b98f6b118
hermberhe <hermberhe@gmail.com>
2025-04-25 15:42:14 -0700

    Homescreen

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
 package src;
 
 public class Main {
+    //private static Object HomeScreen;
+
+    public static void main(String[] args) {
+        Store store = new Store();
+        HomeScreen.displayProducts();  // start the HomeScreen
+    }
 }
